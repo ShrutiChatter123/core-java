2403a14dc914ebb2e7a1de02cc132f1fcb12b1e2
unknown <shrutichatter.xworkz@gmail.com>
2023-01-19 21:05:12 +0530

    commting task files

---
+++ FoodItemsRunner.java
@@ -0,0 +1,90 @@
+class FoodItemsRunner{  
+
+public static void main(String[]args){
+
+double price = FoodItems.full("Panneer tikka");
+System.out.println("Price of food 1 : "+ price);
+
+double price2 = FoodItems.put("role tikka");
+System.out.println("Price of food 1 : "+ price2);
+
+double price3 =FoodItems.cat("chops");
+System.out.println("price of food 3 :"+price3);
+
+double price4 =FoodItems.add("roles");
+System.out.println("price of food 4:"+price4);
+
+double price5 =FoodItems.month("dosa");
+System.out.println("price of food 5:"+price5);
+
+double price6 =FoodItems.year("idli");
+System.out.println("price of food 6:"+price6);
+
+double price7 =FoodItems.access("payasa");
+System.out.println("price of food 7:"+price7);
+
+double price8 =FoodItems.pincode("kurma");
+System.out.println("price of food 8:"+price8);
+
+double price9 = FoodItems.moral("uppit");
+System.out.println("Price of food 9 : "+ price9);
+
+double price10 = FoodItems.process("roti");
+System.out.println("Price of food 10 : "+ price10);
+
+double price11 =FoodItems.mining("cholebutter");
+System.out.println("price of food 11:"+price11);
+
+double price12 =FoodItems.mirror("gobi");
+System.out.println("price of food 12:"+price12);
+
+double price13 =FoodItems.mini("plain juice");
+System.out.println("price of food 13:"+price13);
+
+double price14 =FoodItems.lapy("shavge uppit");
+System.out.println("price of food 14:"+price14);
+
+double price15 =FoodItems.car("palak pannir");
+System.out.println("price of food 15:"+price15);
+
+double price16 =FoodItems.boost("chicken");
+System.out.println("price of food 16:"+price16);
+
+double price17 =FoodItems.fox("fish");
+System.out.println("price of food 17:"+price17);
+
+double price18 =FoodItems.beauty("egg omlet");
+System.out.println("price of food 18:"+price18);
+
+double price19 =FoodItems.cash("pulav");
+System.out.println("price of food 19:"+price19);
+
+double price21 =FoodItems.brave("onion fry");
+System.out.println("price of food 20:"+price21);
+
+double price22 =FoodItems.guest("thali");
+System.out.println("price of food 22:"+price22);
+
+double price23 =FoodItems.beach("fish thali");
+System.out.println("price of food 23:"+price23);
+
+double price24 =FoodItems.bag("tomoto omlet");
+System.out.println("price of food 24:"+price24);
+
+double price25 =FoodItems.click("pulav omlet");
+System.out.println("price of food 19:"+price19);
+
+double price20 =FoodItems.building(" grilled chicken fry");
+System.out.println("price of food 20:"+price20);
+
+
+
+
+}
+
+
+
+
+
+
+}
