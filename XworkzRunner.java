8011e00743829e68426d4df71b4bd61db30b0ddc
unknown <shrutichatter.xworkz@gmail.com>
2023-01-19 21:03:42 +0530

    commiting tsk files

---
+++ XworkzRunner.java
@@ -0,0 +1,81 @@
+class XworkzRunner{
+
+public static void main(String[] args){
+
+
+long see = Xworkz.full("Shruti");
+System.out.println("Mobile number " + see);
+
+long see1 = Xworkz.stack("Murali");
+System.out.println("Mobile number " + see1);
+
+long see2 = Xworkz.qick("Sumith");
+System.out.println("Mobile number " + see2);
+
+long see3 = Xworkz.verb("Bhairavi");
+System.out.println("Mobile number " + see3);
+
+long see4 = Xworkz.dummy("Akashata");
+System.out.println("Mobile number " + see4);
+
+long see5 = Xworkz.access("Darashan");
+System.out.println("Mobile number " + see5);
+
+
+long see6 = Xworkz.visible("Priyanaka");
+System.out.println("Mobile number " + see6);
+
+long see7 = Xworkz.wire("Anita");
+System.out.println("Mobile number " + see7);
+
+long see8 = Xworkz.arrayy("Annesh");
+System.out.println("Mobile number " + see8);
+
+long see9 = Xworkz.bun("Shubangi");
+System.out.println("Mobile number " + see9);
+
+long see10 = Xworkz.veg("Kushal");
+System.out.println("Mobile number " + see10);
+
+long see11 = Xworkz.cream("Shalini");
+System.out.println("Mobile number " + see11);
+
+long see12 = Xworkz.sandwich("Kavana");
+System.out.println("Mobile number " + see12);
+
+long see13 = Xworkz.salt("Basavaraj");
+System.out.println("Mobile number " + see13);
+
+long see14 = Xworkz.cup("Rakaesh");
+System.out.println("Mobile number " + see14);
+
+long see15 = Xworkz.bills("shivukumara");
+System.out.println("Mobile number " + see15);
+
+long see16 = Xworkz.god("Kushal");
+System.out.println("Mobile number " + see16);
+
+long see17 = Xworkz.food("Dinakar");
+System.out.println("Mobile number " + see17);
+
+long see18 = Xworkz.nonVeg("Manju");
+System.out.println("Mobile number " + see18);
+
+long see19 = Xworkz.table("Abhi");
+System.out.println("Mobile number " + see19);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
