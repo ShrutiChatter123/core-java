18ece877ade4f24ebaa1efc75f1d3ab0e42f77e8
unknown <shrutichatter.xworkz@gmail.com>
2023-01-20 18:38:49 +0530

    committing

---
+++ Busroute.java
@@ -0,0 +1,183 @@
+class Busroute{
+	
+	
+	 public static int busRoute(String name){
+		
+		
+		if(name== "bagalkot to belagavi"){
+		System.out.println("route:"+name);
+		int ticket=165;
+		return ticket;
+		}
+		
+		if(name== "belagavi to bagalkot"){
+		System.out.println("route:"+name);
+		int ticket=188;
+		return ticket;
+		}
+		if(name== "rampur to belagavi"){
+		System.out.println("route:"+name);
+		int ticket=200;
+		return ticket;
+		}
+		
+		if(name== "hallur to rampur"){
+		System.out.println("route:"+name);
+		int ticket=75;
+		return ticket;
+		}
+		
+		if(name== "haveri to hasan"){
+		System.out.println("route:"+name);
+		int ticket=335;
+		return ticket;
+		}
+		
+		if(name== "koppal to kalaburgi"){
+		System.out.println("route:"+name);
+		int ticket=232;
+		return ticket;
+		}
+		
+		if(name== "raichur to belagavi"){
+		System.out.println("route:"+name);
+		int ticket=380;
+		return ticket;
+		}
+		
+		if(name== "hubli to hasan"){
+		System.out.println("route:"+name);
+		int ticket=321;
+		return ticket;
+		}
+		
+		if(name== "gajanur to channai"){
+		System.out.println("route:"+name);
+		int ticket=543;
+		return ticket;
+		}
+		
+		if(name== "chikballapur to bidar"){
+		System.out.println("route:"+name);
+		int ticket=444;
+		return ticket;
+		}
+		
+		if(name== "gadag to haveri"){
+		System.out.println("route:"+name);
+		int ticket=227;
+		return ticket;
+		}
+		
+		if(name== "kolar to kolar nagar"){
+		System.out.println("route:"+name);
+		int ticket=275;
+		return ticket;
+		}
+		
+		if(name== "mandya to koppal"){
+		System.out.println("route:"+name);
+		int ticket=280;
+		return ticket;
+		}
+		
+		if(name== "yadgir to hasan"){
+		System.out.println("route:"+name);
+		int ticket=324;
+		return ticket;
+		
+		}
+	
+		if(name== "uttar kannada to koppal"){
+		System.out.println("route:"+name);
+		int ticket=480;
+		return ticket;
+		}
+		if(name== "tumakuru to ramanagar"){
+		System.out.println("route:"+name);
+		int ticket=765;
+		return ticket;
+		}
+	
+		if(name== "kodagu to mysuru"){
+		System.out.println("route:"+name);
+		int ticket=546;
+		return ticket;
+		}
+	
+	if(name== "kalaburagi to kolar"){
+		System.out.println("route:"+name);
+		int ticket=280;
+		return ticket;
+		}
+	
+	if(name== "dharwad to hassan"){
+		System.out.println("route:"+name);
+		int ticket=654;
+		return ticket;
+		}
+	
+	if(name== "haveri to koppal"){
+		System.out.println("route:"+name);
+		int ticket=360;
+		return ticket;
+		}
+	
+		if(name== "gadag to chitradurga"){
+		System.out.println("route:"+name);
+		int ticket=780;
+		return ticket;
+		}
+	
+		if(name== "bagalkot to ballari"){
+		System.out.println("route:"+name);
+		int ticket=220;
+		return ticket;
+		}
+	
+	
+		if(name== "mandya to koppal"){
+		System.out.println("route:"+name);
+		int ticket=87;
+		return ticket;
+		}
+	
+	    if(name== "bangalore to chikkamagaluru"){
+		System.out.println("route:"+name);
+		int ticket=780;
+		return ticket;
+		}
+	
+		if(name== "hampi to hosapeti"){
+		System.out.println("route:"+name);
+		int ticket=65;
+		return ticket;
+		}
+	
+	
+		if(name== "davanagere to ballari"){
+		System.out.println("route:"+name);
+		int ticket=123;
+		return ticket;
+		}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	}	
+		
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ BusrouteRunner.java
@@ -0,0 +1,116 @@
+class BusrouteRunner{
+	
+	public static void main(String[] args){
+		
+		int ref=Busroute.busRoute("bagalkot to belagavi");
+		System.out.println("Route price "+ ref);
+		
+		int ref1=Busroute.busRoute("belagavi to bagalkot");
+		System.out.println("Route price "+ ref1);
+		
+		int ref2=Busroute.busRoute("rampur to belagavi");
+		System.out.println("Route price "+ ref2);
+		
+		int ref3=Busroute.busRoute("hallur to rampur");
+		System.out.println("Route price "+ ref3);
+		
+		int ref4=Busroute.busRoute("haveri to hasan");
+		System.out.println("Route price "+ ref4);
+		
+		int ref5=Busroute.busRoute("koppal to kalaburgi");
+		System.out.println("Route price "+ ref5);
+		
+		int ref6=Busroute.busRoute("raichur to belagavi");
+		System.out.println("Route price "+ ref6);
+		
+		int ref7=Busroute.busRoute("hubli to hasan");
+		System.out.println("Route price "+ ref7);
+		
+		int ref8=Busroute.busRoute("gajanur to channai");
+		System.out.println("Route price "+ ref8);
+		
+		int ref9=Busroute.busRoute("chikballapur to bidar");
+		System.out.println("Route price "+ ref9);
+		
+		int ref10=Busroute.busRoute("kolar to kolar nagar");
+		System.out.println("Route price "+ ref10);
+		
+		int ref11=Busroute.busRoute("mandya to koppal");
+		System.out.println("Route price "+ ref11);
+		
+		int ref12=Busroute.busRoute("yadgir to hasan");
+		System.out.println("Route price "+ ref12);
+		
+		int ref13=Busroute.busRoute("uttar kannada to koppal");
+		System.out.println("Route price "+ ref13);
+		
+		int ref14=Busroute.busRoute("tumakuru to ramanagar");
+		System.out.println("Route price "+ ref14);
+		
+		int ref15=Busroute.busRoute("kodagu to mysuru");
+		System.out.println("Route price "+ ref15);
+		
+		int ref16=Busroute.busRoute("kalaburagi to kolar");
+		System.out.println("Route price "+ ref16);
+		
+		int ref17=Busroute.busRoute("dharwad to hassan");
+		System.out.println("Route price "+ ref17);
+		
+		int ref18=Busroute.busRoute("haveri to koppal");
+		System.out.println("Route price "+ ref18);
+		
+		int ref19=Busroute.busRoute("gadag to chitradurga");
+		System.out.println("Route price "+ ref19);
+		
+		int ref20=Busroute.busRoute("bagalkot to ballari");
+		System.out.println("Route price "+ ref20);
+		
+		int ref21=Busroute.busRoute("bangalore to chikkamagaluru");
+		System.out.println("Route price "+ ref21);
+		
+		int ref22=Busroute.busRoute("hampi to hosapeti");
+		System.out.println("Route price "+ ref22);
+		
+		int ref23=Busroute.busRoute("davanagere to ballari");
+		System.out.println("Route price "+ ref23);
+		
+		int ref24=Busroute.busRoute("gadag to haveri");
+		System.out.println("Route price "+ ref24);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
