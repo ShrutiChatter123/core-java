caab2a230fbfdd023b20fb678907fec5c3a6c25b
unknown <shrutichatter.xworkz@gmail.com>
2023-01-21 09:43:31 +0530

    minimising file

---
+++ CountriesRunner.java
@@ -0,0 +1,14 @@
+class CountriesRunner{
+	
+	public static void main(String[] args){
+		
+		long citycodes=Countries.countryname("india","karanataka","bangalore");
+		System.out.println("city code:"+citycodes);
+		
+		
+	}
+	
+	
+	
+	
+}
